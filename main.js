f4177d3350667839c6ca6c7116a9a05acc94a36d
tpr1992 <terence.pataneronan@gmail.com>
2019-08-22 13:50:04 -0400

    updated 8/22

--- main.js
+++ main.js
@@ -117,3 +117,75 @@ isPalindrome = (str) => {
   let reverseStr = lowRegStr.split('').reverse().join('');
   return reverseStr === lowRegStr;
 }
+
+
+// ########################################################
+//  Collect Odd Values - Pure Recursion
+
+function collectOddValues(arr) {
+  let newArr = [];
+  if (arr.length === 0) {
+    return newArr;
+  }
+  if (arr[0] % 2 !== 0) {
+    newArr.push(arr[0]);
+  }
+  newArr = newArr,concat(collectOddValues(arr.slice(1)));
+  return newArr;
+}
+
+// collectOddValues([1, 2, 3, 4, 5])
+// [1, 3, 5]
+
+
+// ########################################################
+
+//  PowerOf
+//  Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent. This function should mimic the functionality of  Math.pow() - do not worry about negative bases and exponents.
+
+function power(base, exponent) {
+  if (exponent === 0) {
+    return 1
+  }
+  else {
+    return base * power(base, exponent - 1)
+  }
+}
+
+// Shorter Version of the Above Power Methods
+function power(base, exponent){
+    if(exponent === 0) return 1;
+    return base * power(base,exponent-1);
+}
+
+//  Factorial
+//  Write a function called factorial which accepts a number and returns the factorial of that number. A&nbsp;factorial is the product of an integer and all the integers below it; e.g., factorial four (&nbsp; ) is equal to 24, because 4 * 3 * 2 * 1 equals 24.  factorial zero (0!) is always 1.
+function factorial(x){
+   if (x < 0 ) return 0;
+   if (x <= 1 ) return 1;
+   return x * factorial(x-1);
+}
+
+// Product of Array
+// Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+function productOfArray(arr) {
+    if(arr.length === 0) {
+        return 1;
+    }
+    return arr[0] * productOfArray(arr.slice(1));
+}
+
+// Recursive Range Solution
+//  Write a function called  recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function
+function recursiveRange(x){
+   if (x === 0 ) return 0;
+   return x + recursiveRange(x-1);
+}
+
+//  Fibonocci Solution
+// Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence. Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
+
+function fib(n){
+    if (n <= 2) return 1;
+    return fib(n-1) + fib(n-2);
+}
