31cab0a937c16757e75693dbae76306b7e3aa6a9
tpr1992 <terence.pataneronan@gmail.com>
2019-09-25 16:00:45 -0400

    dijkstras and diagonal matrix

--- main.js
+++ main.js
@@ -130,7 +130,7 @@ function chunk(array, size) {
 
 function chunkTwo(array, size) {
   const chunked = [];
-  let index = 0; 
+  let index = 0;
 
   while (index < array.length) {
     chunked.push(array.slice(index, index + size));
@@ -580,3 +580,89 @@ function generateNextRow(lastRow) {
   return nextRow
 }
 console.log(pascalTriangle(4))
+
+
+// ######################################################################################
+
+// Checking for Diagonal Matrix - return boolean
+
+function is_diagonal_matrix(user_matrix) {
+    for (var i = 0; i < user_matrix.length; i++) {
+        for (var j = 0; j < user_matrix.length; j++) {
+            if (i !== j && user_matrix[i][j] !== 0)
+              return false;
+        }
+    }
+    return true;
+}
+
+// ######################################################################################
+
+// Dijkstra's Algorithm Using a Weighted Graph
+
+
+const problem = {
+  start: {A: 5, B: 2},
+  A: {C: 4, D: 2},
+  B: {A: 8, D: 7},
+  C: {D: 6, finish: 3},
+  D: {finish: 1},
+  finish: {}
+};
+
+const lowestCostNode = (costs, processed) => {
+  return Object.keys(costs).reduce((lowest, node) => {
+    if (lowest === null || costs[node] < costs[lowest]) {
+      if (!processed.includes(node)) {
+        lowest = node;
+      }
+    }
+    return lowest;
+  }, null);
+};
+
+// function that returns the minimum cost and path to reach Finish
+const dijkstra = (graph) => {
+  // track lowest cost to reach each node
+  const costs = Object.assign({finish: Infinity}, graph.start);
+  // track paths
+  const parents = {finish: null};
+  for (let child in graph.start) {
+    parents[child] = 'start';
+  }
+  // track nodes that have already been processed
+  const processed = [];
+  let node = lowestCostNode(costs, processed);
+
+  while (node) {
+    let cost = costs[node];
+    let children = graph[node];
+    for (let n in children) {
+      let newCost = cost + children[n];
+      if (!costs[n]) {
+        costs[n] = newCost;
+        parents[n] = node;
+      }
+      if (costs[n] > newCost) {
+        costs[n] = newCost;
+        parents[n] = node;
+      }
+    }
+    processed.push(node);
+    node = lowestCostNode(costs, processed);
+  }
+  let optimalPath = ['finish'];
+  let parent = parents.finish;
+  while (parent) {
+    optimalPath.push(parent);
+    parent = parents[parent];
+  }
+  optimalPath.reverse();
+  const results = {
+    distance: costs.finish,
+    path: optimalPath
+  };
+  return results;
+};
+
+console.log(dijkstra(problem));
