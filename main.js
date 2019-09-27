5f714bd1bbd924b85abfb68d78bd495d2d23018d
tpr1992 <terence.pataneronan@gmail.com>
2019-09-26 21:42:11 -0400

    linkedlist practice

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//  Algo Gang is brought to you by Terence, Jamil, John, and Miguel
+kkhkjbkjhkjhkj//  Algo Gang is brought to you by Terence, Jamil, John, and Miguel
 
 
 //  String to lower case
@@ -666,3 +666,103 @@ const dijkstra = (graph) => {
 };
 
 console.log(dijkstra(problem));
+
+// ########################################################################################
+
+// Linked List Practice, initial creation + basic functions like search and indexOf
+
+
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+LinkedList.prototype.addToHead = function(value) {
+  var newNode = new Node(value, this.head, null);
+  if (this.head) {
+    this.head.prev = newNode;
+  } else {
+      this.tail = newNode;
+  }
+  this.head = newNode;
+}
+
+var ll = new LinkedList();
+
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value, null, this.tail);
+  if (this.tail) {
+    this.tail.next = newNode;
+  } else {
+    this.head = newNode;
+  }
+  this.tail = newNode;
+}
+
+LinkedList.prototype.removeHead = function() {
+  if (!this.head) {
+    return null
+  }
+  var val = this.head.value;
+  this.head = this.head.next;
+  if (this.head) {
+    this.head.prev = null;
+  } else {
+    this.tail = null;
+  }
+  return val;
+}
+
+LinkedList.prototype.removeTail = function() {
+  if (!this.tail) {
+    return null;
+  }
+  var val = this.tail.value;
+  this.tail = this.tail.prev;
+  if (this.tail) {
+    this.tail.next = null;
+  } else {
+    this.head = null;
+  }
+  return val;
+}
+
+LinkedList.prototype.search = function(searchValue) {
+  var currentNode = this.head;
+  while (currentNode) {
+    if (currentNode.value === searchValue) return currentNode.value;
+    currentNode = currentNode.next;
+  }
+  return null;
+}
+
+LinkedList.prototype.indexOf = function(val) {
+  let currentIndex = 0;
+  let indexes = [];
+  let currentNode = this.head;
+  while (currentNode) {
+    if (currentNode.value === val) {
+      indexes.push(currentIndex);
+    }
+    currentNode = currentNode.next;
+    currentIndex++;
+  }
+  return indexes;
+}
+
+var ll = new LinkedList();
+
+ll.addToTail(1);
+ll.addToTail(5);
+ll.addToTail(3);
+ll.addToTail(5);
+ll.addToTail(8);
+ll.addToTail(7);
+ll.addToTail(5);
+console.log(ll.indexOf(5));
