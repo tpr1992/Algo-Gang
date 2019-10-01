d7bb9602f6c5a76782f50d06767853635dc08ddb
tpr1992 <terence.pataneronan@gmail.com>
2019-10-01 14:44:16 -0400

    palindrome alt

--- main.js
+++ main.js
@@ -169,6 +169,20 @@ isPalindrome = (str) => {
   return reverseStr === lowRegStr;
 }
 
+//  For Loop
+
+function palindrome(str) {
+ var re = /[^A-Za-z0-9]/g;
+ str = str.toLowerCase().replace(re, '');
+ var len = str.length;
+ for (var i = 0; i < len/2; i++) {
+   if (str[i] !== str[len - 1 - i]) {
+       return false;
+   }
+ }
+ return true;
+}
+
 
 // ########################################################
 //  Collect Odd Values - Pure Recursion
