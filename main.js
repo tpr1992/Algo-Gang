8f4fe52d77752930ee475eb64592d3d996ae75ac
tpr1992 <terence.pataneronan@gmail.com>
2019-10-15 22:04:49 -0400

    greatest common and alt pascal

--- main.js
+++ main.js
@@ -780,3 +780,35 @@ ll.addToTail(8);
 ll.addToTail(7);
 ll.addToTail(5);
 console.log(ll.indexOf(5));
+
+// #####################################################################################
+
+// --------------------------------------------------------
+
+// Greatest Common Divisor
+
+function greatestCommonDivisor(a, b){
+  var divisor = 2,
+  greatestDivisor = 1;
+
+  //if u pass a -ve number this will not work. fix it dude!!
+  if (a < 2 || b < 2)
+  return 1;
+
+  while(a >= divisor && b >= divisor){
+    if(a %divisor == 0 && b% divisor ==0){
+      greatestDivisor = divisor;
+    }
+    divisor++;
+  }
+  return greatestDivisor;
+}
+
+// Alt
+function greatestCommonDivisor(a, b){
+  if(b == 0) {
+    return a;
+  } else {
+    return greatestCommonDivisor(b, a%b);
+  }
+}
--- pascal.js
+++ pascal.js
@@ -37,3 +37,25 @@ function generateNextRow(lastRow) {
   return nextRow
 }
 console.log(pascalTriangle(10))
+
+
+// ------------------------------------------------------
+
+// Pascal ver 2 - quicker (found online)
+
+/**
+* @param {number} lineNumber - zero based.
+* @return {number[]}
+*/
+export default function pascalTriangle(lineNumber) {
+  const currentLine = [1];
+
+  const currentLineSize = lineNumber + 1;
+
+  for (let numIndex = 1; numIndex < currentLineSize; numIndex += 1) {
+    // See explanation of this formula in README.
+    currentLine[numIndex] = currentLine[numIndex - 1] * (lineNumber - numIndex + 1) / numIndex;
+  }
+
+  return currentLine;
+}
