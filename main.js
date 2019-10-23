35b2212f1f6415e6e5fa8d4a1027939df2deeb49
tpr1992 <terence.pataneronan@gmail.com>
2019-10-23 15:42:13 -0400

    selection sort

--- main.js
+++ main.js
@@ -1073,3 +1073,69 @@ function bubbleSort(arr) {
 }
 
 bubbleSort([8,1,2,3,4,5,6,7])
+
+
+// ###################
+// Optimized Bubble Sort w/ noSwaps
+
+function bubbleSort(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+bubbleSort([8,1,2,3,4,5,6,7]);
+
+
+
+// ####################################################################################
+// Selection Sort
+
+// LEGACY VERSION (non ES2015 syntax)
+function sselectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      //SWAP!
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+
+// Alt Version
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) =>
+  ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]])
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest)
+  }
+  return arr
+}
+
+selectionSort([0,2,34,22,10,19,17]);
